da019c43b53106bb8b67c0f141595f3f813d80a5
rakarahadyan <rakarahadyan177@gmail.com>
2025-09-06 10:50:02 +0700

    Refactor cors header

--- config/database.php
+++ config/database.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $host = "localhost";
 $db_name = "dblms";
 $username = "root";
